09d983a90fb6b40f5b4318c079271b7dd5521a03
Jonas Pinheiro <jonaspinheiro777@gmail.com>
2022-10-18 21:12:08 -0300

    Script de criação e remoção de usuário

---
+++ rmuser.sh
@@ -0,0 +1,30 @@
+!#/bin/bash
+
+
+
+#Removendo diretorios
+
+
+rm -r /publico
+rm -r /adm
+rm -r /ven
+rm -r /sec
+
+#Removendo grupos
+
+groupdel GRP_ADM
+groupdel GRP_VEN
+groupdel GRP_SEC
+
+#Removendo usuários
+
+userdel -r carlos
+userdel -r maria
+userdel -r joao
+userdel -r debora
+userdel -r sebastiana
+userdel -r roberto
+userdel -r josefina
+userdel -r amanda
+userdel -r rogerio
+
